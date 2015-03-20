83ad76099caf373790f0796b8d1faa680c0ffb23
unknown <lhotte.lionel@P6322-06.SIO2>
2015-03-20 11:18:48 +1100

    Fonction d'ajout variété de noix
    Signed-off-by: unknown <lhotte.lionel@P6322-06.SIO2>

--- ajouter.php
+++ ajouter.php
@@ -6,11 +6,10 @@
 	$dbname="agrur";
 	$bdd = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pwd", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 	$nomVariete="$_POST('nomNoix')";
-	$nomVariete="$_POST('description')";
-	$nomVariete="$_POST('choix')";
-	$sql="INSERT INTO client VALUES ('',?, 'Chef de guerre', '$lieu', ?)";
+	$AOC="$_POST('choix')";
+	$sql="INSERT INTO variete VALUES ($nomVariete, $AOC )";
 	$oRS = mysqli_prepare($bdd,$sql);
-	if(mysqli_stmt_bind_param($oRS,'si',$per_Nom,$per_Niveau)){ // s pour les chaînes de caractères / i pour les entiers / d pour les décimaux / b pour les BLOB
+	if(mysqli_stmt_bind_param($oRS,'si',$nomVariete,$AOC)){ // s pour les chaînes de caractères / i pour les entiers / d pour les décimaux / b pour les BLOB
 		if(mysqli_stmt_execute($oRS)){
 			echo "Requête effectué avec succès !";
 		}
