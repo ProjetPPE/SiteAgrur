2c152d4a4bf06d9845fdb4a8b71975ad9b51d187
JudgeBibi <SeigneurBibi@hotmail.fr>
2015-03-27 10:13:15 +0100

    Amélioration des titres du tableau

--- afficher.php
+++ afficher.php
@@ -9,7 +9,7 @@
 		$sql="select * from client";
 		if($oRS = mysqli_query($bdd,$sql)){
 			if(mysqli_num_rows($oRS) > 0 ){
-				echo "<table id='tableau'><thead><tr><th id='tableau'>Client</th><th id='tableau'>Nom Responsable Client</th><th id='tableau'>Adresse Client</th></tr></thead>";
+				echo "<table id='tableau'><thead><tr><th id='tableau'>Client</th><th id='tableau'>Nom Du Responsable</th><th id='tableau'>Adresse Du Client</th></tr></thead>";
 				while ($data = mysqli_fetch_assoc($oRS)){
 					echo "<tr ><td id='tableau'>".$data['clientNom']."</td><td id='tableau'>".$data['clientNomResp']."</td><td id='tableau'>".$data['clientAdresse']."</td></tr>";
 				}
