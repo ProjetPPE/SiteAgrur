729d21661ffb2d677d35261eaa072964865c2ba3
JudgeBibi <SeigneurBibi@hotmail.fr>
2015-03-27 09:57:38 +0100

    Consultation fonctionnelle (mysqli)

--- connexion.php
+++ connexion.php
@@ -4,27 +4,27 @@
 	$pwd="root";
 	$host="localhost";
 	$dbname="agrur";
-	$bdd = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pwd", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	$bdd = mysqli_connect ($host,$user,$pwd,$dbname) or die("Erreur de connexion à la base de données :".mysqli_error($link));
 	if($bdd){
 		$sql="select * from client";
-		if($oRS = $bdd->query($sql)){ 
-			if (count($oRS->fetchAll()) > 0 ){
+		if($oRS = mysqli_query($bdd,$sql)){
+			if(mysqli_num_rows($oRS) > 0 ){
 				echo "<table><thead><tr><th>Client</th><th>Nom Responsable Client</th><th>Adresse Client</th></tr></thead><tbody>";
-				while($data = $oRS ->fetch()){
-					echo $data;
-					echo "coucou";
-					echo "<tr><td>".$data['clientId']."</td><td>".$data['clientNom']."</td><td>".$data['clientNomResp']."</td><td>".$data['clientAdresse']."</td></tr>";	
+				while ($data = mysqli_fetch_assoc($oRS)){
+					echo "<tr><td>".$data['clientId']."</td><td>".$data['clientNom']."</td><td>".$data['clientNomResp']."</td><td>".$data['clientAdresse']."</td></tr>";
 				}
 				echo "</tbody></table>";
 			}
 			else {
-				echo "aucun client";
+				echo "client.";
 			}
 		}
 		else {
 			echo "echec requete";
 		}
 	}
+	
 	$sSQL = "";
 	$oRS = NULL;
+	mysqli_close($bdd);
 ?>
