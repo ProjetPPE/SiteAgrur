b1713982c4b849cad6b774ec34b75639b9e842a9
JudgeBibi <sio1415@P6322-06.SIO2>
2015-04-03 08:11:38 +1100

    Vue SQL
    exemple de vue SQL

---
+++ exemple-vue.sql
@@ -0,0 +1,12 @@
+CREATE VIEW lesVergers(col1,col2,col3,col4,col5)
+AS SELECT vergerSuperficie, vergerNbArbre, prodID, varID, commuID
+FROM
+
+
+CREATE VIEW Vue
+(colonneA,colonneB,colonneC,colonneD)
+AS SELECT colonne1,colonne2,colonneI,colonneII
+FROM Nom_table1 Alias1,Nom_tableII AliasII
+WHERE Alias1.colonne1 = AliasII.colonneI
+AND Alias1.colonne2 = AliasII.colonneII
+['vergerSuperficie']['vergerNbArbre']['prodID']['varID']['commuID']
