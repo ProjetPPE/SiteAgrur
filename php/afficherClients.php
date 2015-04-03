6f8954185e3ee1519bc8f64dfd0b11fb8dc349d8
JudgeBibi <lhotte.l@live.fr>
2015-04-03 11:26:39 +1100

    Test connexion
    modification mdp pour connexion wamp classique

--- php/afficherClients.php
+++ php/afficherClients.php
@@ -1,7 +1,7 @@
 <?php
 	/*** Connexion BDD Agrur ***/
-	$user="root";
-	$pwd="root";
+	$user="admin"; //root si uWamp
+	$pwd=""; //root si uWamp
 	$host="localhost";
 	$dbname="agrur";
 	$bdd = mysqli_connect ($host,$user,$pwd,$dbname) or die("Erreur de connexion à la base de données :".mysqli_error($link));
