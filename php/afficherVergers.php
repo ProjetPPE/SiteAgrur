e73028ed0d4ff55188e3be09d57aa6d7d5151609
JudgeBibi <SeigneurBibi@hotmail.fr>
2015-04-29 12:13:49 +0200

    Amélioration de la requête d'affichage

--- php/afficherVergers.php
+++ php/afficherVergers.php
@@ -2,12 +2,16 @@
 	include_once("php/fonctions.php"); //include_once permet d'inclure une seule fois le fichier (évite le fatal error de php5)
 	
 	if($bd = connecter()){
-		$sql="select * from verger";
+		$sql="select producteur.nomSociete as producteur, superficiePlantee, nbArbresHect, libelleVariete, commune.nom as commune
+			  from verger, producteur, commune
+			  where verger.idProducteur = producteur.idProducteur
+			  and verger.idCommune = commune.idCommune";
+		
 		if($oRS = mysqli_query($bd,$sql)){
 			if(mysqli_num_rows($oRS) > 0 ){
-				echo "<table id='tableau'><thead><tr><th id='tableau'>Superficie du Verger</th><th id='tableau'>Nombre d'arbres</th><th id='tableau'>Producteur</th><th id='tableau'>Variété du verger</th><th id='tableau'>Commune</th></tr></thead>";
+				echo "<table id='tableau'><thead><tr><th id='tableau'>Commune</th><th id='tableau'>Producteur</th><th id='tableau'>Variété du verger</th><th id='tableau'>Superficie du Verger</th><th id='tableau'>Nombre d'arbres</th></tr></thead>";
 				while ($data = mysqli_fetch_assoc($oRS)){
-					echo "<tr ><td id='tableau'>".$data['superficiePlantee']."</td><td id='tableau'>".$data['nbArbresHect']."</td><td id='tableau'>".$data['idProducteur']."</td><td id='tableau'>".$data['libelleVariete']."</td><td id='tableau'>".$data['idCommune']."</td></tr>";
+					echo "<tr ><td id='tableau'>".$data['commune']."</td><td id='tableau'>".$data['producteur']."</td><td id='tableau'>".$data['libelleVariete']."</td><td id='tableau'>".$data['superficiePlantee']."</td><td id='tableau'>".$data['nbArbresHect']."</td></tr>";
 				}
 				echo "</table>";
 			}
